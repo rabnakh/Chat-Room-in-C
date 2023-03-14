2435da81a8a54f12966564404323cbdd61ec64c3
rabnakh <xurizo903@gmail.com>
2023-03-13 19:08:18 -0500

    Added bash script file to create the database

---
+++ createDatabase.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+mkdir -p ./Database/{"Chat_Thread_Names","Thread_Chat_Logs","Users_Info"}
+touch Database/Chat_Thread_Names/chat_thread_names.txt
+touch Database/Thread_Chat_Logs/thread_{1..8}.txt
+touch Database/Users_Info/users_cred.txt
